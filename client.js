ae964bae01a12958958600ad0ea5be7e5af12ba5
Arjun <arjun071194@gmail.com>
2024-12-28 16:13:55 +0530

    included readline module for handling inputs and outputs from CLI

--- client.js
+++ client.js
@@ -1,9 +1,24 @@
 const net = require("net");
+// to handle input and output streams for reading input from users (via the command line) and writing output
+const readline = require("readline/promises");
 
-const socket = net.createConnection({
-  host: "127.0.0.1",
-  port: 3008,
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+
+const socket = net.createConnection(
+  {
+    host: "127.0.0.1",
+    port: 3008,
+  },
+  async () => {
+    console.log("connected to the server");
+
+    const message = await rl.question("Enter a message >");
+    socket.write(message);
+  }
+);
 
 socket.on("end", () => {
   console.log("connection was ended");
