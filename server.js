de6f05eb9c3186ce091fc573c9633e4f2df7f50a
Arjun <arjun071194@gmail.com>
2024-12-28 16:47:03 +0530

    since each socket is connected to particular client even though many client can connect to server. It is not well suited for chat room where in messages of all clients should be visible to all clients console. So created an array and socket (connection) will be pushed everytime new client comes. From that array we are sending all the messages to the all sockets by looping through it and writing the message individually to every socket connected.

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const net = require("net");
 
 const server = net.createServer();
 
+// all new sockets(clients) will pushed in to this array
+const clients = [];
+
 server.on("connection", (socket) => {
   socket.on("data", (data) => {
-    socket.write(data);
+    clients.map((s) => {
+      s.write(data);
+    });
   });
+  clients.push(socket);
 });
 
 server.listen(3008, "127.0.0.1", () => {
